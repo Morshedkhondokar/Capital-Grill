4dd72ba3de6f2141f995507def52e221a1a97591
Morshedkhondokar <morshedkhondokar99@gmail.com>
2025-09-08 18:02:26 +0600

    save food to localStorage

--- src/provider/CartProvider.jsx
+++ src/provider/CartProvider.jsx
@@ -1,10 +1,19 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CartContext from '../context/CartContext';
 
 const CartProvider = ({children}) => {
-    const [cartItems, setCartItems] = useState([])
+    const [cartItems, setCartItems] = useState(() => {
+    // get cartItems if localstorage has cartItems
+    const savedCart = localStorage.getItem("cartItems");
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
     console.log(cartItems,cartItems.length)
+
+// save to localStorage when cartItems change
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
 
     const cartInfo = {
         cartItems,
